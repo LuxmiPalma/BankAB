b7d4ee7ffe1a7e3ed1ff8c4a9653d81cf2c57796
LuxmiPalma <luxmi.palma@gmail.com>
2025-03-21 23:27:40 +0100

    push

--- BankAB/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BankAB/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -124,7 +124,16 @@ namespace BankAB.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    
+                    if (Input.UserRole == null)
+                    {
+                        await _userManager.AddToRoleAsync(
+                            user, "Cashier");
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.UserRole);
+                    }
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
