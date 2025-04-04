09a4000898e21204d01e948f4141e7e8e753414a
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-04 10:21:35 +0200

    push

--- BankAB/Pages/Category.cshtml.cs
+++ BankAB/Pages/Category.cshtml.cs
@@ -17,8 +17,24 @@ namespace BankAB.Pages
         public List<string> GroupValues { get; set; } = new();
         public bool ShowGroups { get; set; }
 
-        public void OnGet()
+        public void OnGet(string groupBy)
+
         {
+            if (!string.IsNullOrEmpty(groupBy))
+            {
+                ShowGroups = true;
+                SelectedGroupBy = groupBy;
+
+                var customers = _customerService.GetCustomers();
+
+                GroupValues = groupBy switch
+                {
+                    "City" => customers.Where(c => !string.IsNullOrEmpty(c.City)).Select(c => c.City).Distinct().OrderBy(c => c).ToList(),
+                    "Country" => customers.Where(c => !string.IsNullOrEmpty(c.Country)).Select(c => c.Country).Distinct().OrderBy(c => c).ToList(),
+                    "Gender" => customers.Where(c => !string.IsNullOrEmpty(c.Gender)).Select(c => c.Gender).Distinct().OrderBy(c => c).ToList(),
+                    _ => new List<string>()
+                };
+            }
         }
     }
 }
