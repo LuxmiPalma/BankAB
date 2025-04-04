dbadd856076d7427a945de347e71f68c026a4784
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-04 10:20:38 +0200

    push

--- BankAB/Pages/Category.cshtml.cs
+++ BankAB/Pages/Category.cshtml.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services;
 
 namespace BankAB.Pages
 {
     public class CategoryModel : PageModel
     {
+        private readonly ICustomerService _customerService;
+
+        public CategoryModel(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public string SelectedGroupBy { get; set; }
+        public List<string> GroupValues { get; set; } = new();
+        public bool ShowGroups { get; set; }
+
         public void OnGet()
         {
         }
