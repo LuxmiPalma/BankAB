f22a5d39a2463d321f4042174c6031bbc47e1195
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-04 16:59:15 +0200

    push

---
+++ BankAB/Infrastructure/Paging/ExtensionMethods.cs
@@ -0,0 +1,6 @@
+﻿namespace BankAB.Infrastructure.Paging
+{
+    public class ExtensionMethods
+    {
+    }
+}
---
+++ BankAB/Infrastructure/Paging/PagedResult.cs
@@ -0,0 +1,7 @@
+﻿namespace BankAB.Infrastructure.Paging
+{
+    public class PagedResult<T> : PagedResultBase where T : class
+    {
+        public IList<T> Results { get; set; } = new List<T>();
+    }
+}
