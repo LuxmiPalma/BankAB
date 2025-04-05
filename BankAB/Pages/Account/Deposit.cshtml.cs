e28a35320074eaeadb4e0a8407f586a5c1c7e06e
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-05 12:15:23 +0200

    push

--- BankAB/Pages/Account/Deposit.cshtml.cs
+++ BankAB/Pages/Account/Deposit.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using DataAccessLayer.Models;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace BankAB.Pages.Account
@@ -13,8 +14,29 @@ namespace BankAB.Pages.Account
         {
             _context = context;
         }
-        public void OnGet()
+        [BindProperty]
+        [Range(100, 10000)]
+        public decimal Amount { get; set; }
+
+        [BindProperty]
+        public DateTime DepositDate { get; set; } = DateTime.Now;
+
+        [BindProperty]
+        [Required, MinLength(5), MaxLength(250)]
+        public string Comment { get; set; }
+
+        public IActionResult OnPost(int accountId)
         {
+            if (!ModelState.IsValid) return Page();
+
+            var account = _context.Accounts.FirstOrDefault(a => a.AccountId == accountId);
+            if (account == null) return NotFound();
+
+            account.Balance += Amount;
+            _context.SaveChanges();
+
+            return RedirectToPage("/Accounts");
         }
     }
 }
+
