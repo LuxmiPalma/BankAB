ed48ce486bca4e718faf628cdb07db9b370a7f28
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-05 12:16:43 +0200

    push

---
+++ BankAB/Pages/Account/Withdraw.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccessLayer.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BankAB.Pages.Account
+{
+    public class WithdrawModel : PageModel
+    {
+        private readonly BankAppDataContext _context;
+
+        public WithdrawModel(BankAppDataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        [Range(100, 10000)]
+        public decimal Amount { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
