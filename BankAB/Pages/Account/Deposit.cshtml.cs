a98430d874ca1f3959144148edac9557881a5591
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-05 12:14:14 +0200

    push

---
+++ BankAB/Pages/Account/Deposit.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccessLayer.Models;
+
+
+namespace BankAB.Pages.Account
+{
+    public class DepositModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
