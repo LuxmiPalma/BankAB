c8fff9f04943e79d2059b5d1eb4e29e062f81182
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-07 12:55:48 +0200

    push

---
+++ Services/IPersonService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+
+
+namespace Services
+{
+    public interface IPersonService
+    {
+        Task<(Customer, List<string>)> UpdateCustomerAsync(
+            int id, string gender, string givenName, string surname, string streetAddress, string city, string zipcode,
+            string country, string countryCode, string emailaddress, string telephoneCountryCode, string telephoneNumber,
+            string? nationalId, int birthdayYear, int birthdayMonth, int birthdayDay);
+
+        Task<Customer> GetCustomerAsync(int id);
+    }
+}
