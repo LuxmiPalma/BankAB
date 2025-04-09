b1085c9b581bc631d6ea8ec89fa43f8b7c80b9f0
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-09 10:35:52 +0200

    push

---
+++ BankAB/Pages/Transactions/TransactionDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankAB.Pages.Transactions
+{
+    public class TransactionDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
