01d180d551cddda2a6c77eae9834cf80f80d8368
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-09 18:30:22 +0200

    Removed duplicated ViewModels and Infrastructure folders from BankAB

--- BankAB/Infrastructure/Paging/PagedResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BankAB.Infrastructure.Paging
-{
-    public class PagedResult<T> : PagedResultBase where T : class
-    {
-        public IList<T> Results { get; set; } = new List<T>();
-    }
-}
