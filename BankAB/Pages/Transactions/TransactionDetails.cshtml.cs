ee271205bf7a3887887bc4bce213e08423c5b407
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-09 10:36:48 +0200

    push

--- BankAB/Pages/Transactions/TransactionDetails.cshtml.cs
+++ BankAB/Pages/Transactions/TransactionDetails.cshtml.cs
@@ -1,10 +1,20 @@
+using BankAB.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services;
 
 namespace BankAB.Pages.Transactions
 {
     public class TransactionDetailsModel : PageModel
     {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionDetailsModel(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        public TransactionViewModel Transaction { get; set; }
         public void OnGet()
         {
         }
