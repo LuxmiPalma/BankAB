32d67250b85f5de7fcc892f9b07eebd58eb1d0ae
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-15 11:48:04 +0200

    push

--- BankAB/Pages/Transactions/Transfer.cshtml.cs
+++ BankAB/Pages/Transactions/Transfer.cshtml.cs
@@ -1,12 +1,33 @@
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace BankAB.Pages.Transactions
 {
     public class TransferModel : PageModel
     {
-        public void OnGet()
+        private readonly BankAppDataContext _context;
+
+        public TransferModel(BankAppDataContext context)
         {
+            _context = context;
         }
+
+        [BindProperty(SupportsGet = true)]
+        public int AccountId { get; set; }
+
+        [BindProperty]
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Target account ID is required")]
+        public int TargetAccountId { get; set; }
+
+        [BindProperty]
+        public string? Comment { get; set; }
+
     }
 }
