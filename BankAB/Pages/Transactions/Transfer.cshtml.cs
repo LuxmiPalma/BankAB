54654556bf8d68acbc13b41c16266cb39a0e5937
LuxmiPalma <luxmi.palma@gmail.com>
2025-04-15 11:43:07 +0200

    push

---
+++ BankAB/Pages/Transactions/Transfer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankAB.Pages.Transactions
+{
+    public class TransferModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
