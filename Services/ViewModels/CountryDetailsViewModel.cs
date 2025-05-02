8310e78594e6c592cf4bd0c81dd25342e61b6739
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-02 10:32:05 +0200

    adding CountryDetailsViewModel

---
+++ Services/ViewModels/CountryDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ViewModels
+{
+    public class CountryDetailsViewModel
+    {
+        public int CustomerId { get; set; }
+        public string Givenname { get; set; }
+        public string Surname { get; set; }
+        public decimal TotalBalance { get; set; }
+    }
+}
