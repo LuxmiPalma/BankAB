1528800516b48a208d9226f187ef6d910e2c4d95
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-05 19:19:33 +0200

    implement weatherview

---
+++ BankAB/Pages/ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankAB.Pages.ViewComponents
+{
+    public class WeatherViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new WeatherModel
+            {
+                Location = "Stockholm",
+                Temperature = "25"
+            };
+
+            return View(model);
+        }
+    }
+    public class WeatherModel
+    {
+        public string Location { get; set; }
+        public string Temperature { get; set; }
+    }
+}
