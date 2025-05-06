617c3d426a5e87972c32bcbc5229ce0afed04472
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-06 21:27:06 +0200

    commit

--- BankAB/Pages/Users/ManageUsers.cshtml.cs
+++ BankAB/Pages/Users/ManageUsers.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,8 @@ namespace BankAB.Pages.Users
 {
     public class ManageUsersModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
         public void OnGet()
         {
         }
