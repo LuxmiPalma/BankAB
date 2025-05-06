025909229f60af24ce2ea298d7bc5838da18d71a
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-06 21:51:12 +0200

    push

--- BankAB/Pages/Users/EditUser.cshtml.cs
+++ BankAB/Pages/Users/EditUser.cshtml.cs
@@ -8,6 +8,18 @@ namespace BankAB.Pages.Users
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+
+        public EditUserModel(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IdentityUser User { get; set; }
+        public List<string> AllRoles { get; set; }
+
+        [BindProperty]
+        public string SelectedRole { get; set; }
         public void OnGet()
         {
         }
